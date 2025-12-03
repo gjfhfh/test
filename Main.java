b2a4de097d2b79133fd2fa8da6ca9fda22338072
gjfhfh <94845372+gjfhfh@users.noreply.github.com>
2025-12-03 03:22:33 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static class FastReader {
+
+        BufferedReader reader;
+        StringTokenizer tokenizer;
+
+        private FastReader() {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        private String next() {
+            while (tokenizer == null || !tokenizer.hasMoreElements()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        private int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        private long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        private double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        private String nextLine() {
+            String str = "";
+            try {
+                if (tokenizer.hasMoreTokens()) {
+                    str = tokenizer.nextToken("\n");
+                } else {
+                    str = reader.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        FastReader in = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+        out.flush();
+    }
+}
